aedb57cfa4e824bb042dfecd0889d13d4e3556ef
Ayusk-onbu <k024g0103@g.neec.ac.jp>
2025-10-23 14:56:13 +0900

    FPS固定実装

--- project/UsefulTool/Time/Chronos.cpp
+++ project/UsefulTool/Time/Chronos.cpp
@@ -1,15 +1,53 @@
 #include "Chronos.h"
 #include "ImGuiManager.h"
+#include "Log.h"
+#include <iostream>
+#include <thread>
 
-long long Chronos::frameCount_ = 0;
-std::chrono::high_resolution_clock::time_point Chronos::lastTime_ = {};
-std::chrono::high_resolution_clock::time_point Chronos::currentTime_ = {};
-long long Chronos::fps_;
+#pragma comment(lib,"winmm.lib")
+
+std::unique_ptr<Chronos> Chronos::instance_ = nullptr;
+
+void Chronos::SetTargetFPS(float targetFPS) {
+	if (targetFPS > 0.0f) {
+		targetFPS_ = targetFPS;
+
+		long long microsec = static_cast<long long>(1000000.0f / targetFPS_);
+		kMinTime_ = std::chrono::microseconds(microsec);
+		microsec = static_cast<long long>(1000000.0f / (targetFPS_ + 5.0f));
+		kMinCheckTime_ = std::chrono::microseconds(microsec);
+		isFixedFPS_ = true;
+		std::cout << "TargetFPS set :" << targetFPS_ << "(" << microsec << "microseconds per frame)" << std::endl;
+	}
+	else {
+		isFixedFPS_ = false;
+		std::cout << "FPS fixed rate disabled." << std::endl;
+	}
+}
+
+void Chronos::Initialize()
+{
+	timeBeginPeriod(1);
+	// FPS計測用の初期化
+	lastTime_ = std::chrono::high_resolution_clock::now();
+	frameCount_ = 0;
+	fps_ = 0;
+
+	// FPS固定用の初期化
+	referenceTime_ = std::chrono::steady_clock::now();
+	SetTargetFPS(63.0f);
+	Log::ViewFile("Chronos Initialized");
+}
 
 void Chronos::Update() {
+	FixedUpdate();
+	CalculateFPS();
+}
+
+void Chronos::CalculateFPS() {
 	frameCount_++;
 
-	currentTime_ = std::chrono::high_resolution_clock::now();
+	std::chrono::high_resolution_clock::time_point currentTime_ = std::chrono::high_resolution_clock::now();
 	auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(currentTime_ - lastTime_);
 
 	if (elapsed_time.count() >= 1.0) {
@@ -19,9 +57,28 @@ void Chronos::Update() {
 		frameCount_ = 0;
 		lastTime_ = currentTime_;
 	}
-#ifdef _DEBUG
-	/*ImGui::Begin("FPS");
-	ImGui::Text("FPS: %lld", fps_);
-	ImGui::End();*/
-#endif // DEBUG
+}
+
+void Chronos::FixedUpdate() {
+	if (!isFixedFPS_) {
+		return;
+	}
+	// 今の時間を入手
+	auto now = std::chrono::steady_clock::now();
+
+	// １Frameにかかった時間を計算
+	auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - referenceTime_);
+
+	// 待つ
+	if (elapsed < kMinCheckTime_) {
+		while (std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - referenceTime_) < kMinTime_) {
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+	}
+
+	referenceTime_ = std::chrono::steady_clock::now();
+}
+
+void Chronos::ChangeIsFixed() {
+	isFixedFPS_ = isFixedFPS_ == true ? false : true;
 }
