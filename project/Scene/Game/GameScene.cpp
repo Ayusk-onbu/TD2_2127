727883266fb1f12ef174bb50d0105ecf0439c12d
yutaka110 <k024g0111@g.neec.ac.jp>
2025-10-23 14:45:28 +0900

    stageeditorの追加途中

--- project/Scene/Game/GameScene.cpp
+++ project/Scene/Game/GameScene.cpp
@@ -1,7 +1,9 @@
 #include "GameScene.h"
 #include "ImGuiManager.h"
 #include "CameraSystem.h"
-
+#include "Transition/TransitionHub.h"
+#include "stageeditor/Stageeditor.h"
+#include "InputManager.h"
 GameScene::~GameScene() {
 	
 	Log::ViewFile("Path GameScene ~");
@@ -18,6 +20,15 @@ void GameScene::Update(){
 	
 	CameraSystem::GetInstance()->Update();
 
+	auto& key = InputManager::GetKey();
+	if (key.PressedKey(DIK_F2)) {
+		Fngine* eng = /* ここはあなたの環境で取得する */ p_fngine_; // 例
+		Transition::FadeToSlideRight([eng] {
+			auto* ed = new StageEditor();
+			ed->BindEngine(eng);       // ★ 必須：StageEditorにエンジンを渡す
+			return ed;
+			}, 0.8f, 0.8f);
+	}
 }
 
 void GameScene::Draw() {
